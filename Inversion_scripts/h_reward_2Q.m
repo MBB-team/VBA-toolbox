aa8760f989fdbcd4227072fabf83ecd698068adc
vincentadam87 <vincent.adam87@gmail.com>
2012-08-08 17:49:30 +0000

    Reorganise

--- Inversion_scripts/h_reward_2Q.m
+++
@@ -1,3 +0,0 @@
-function [fb] = h_reward_2Q(yt,t,in)
-% compares the entry yt with a stored reference answer u0(t)
-fb = in.u0(yt+1,t);
--- Parametric_Modulation/binary_cart_prod.m
+++
@@ -1,8 +0,0 @@
-
-function o = binary_cart_prod(N_p)
- o = zeros(2^N_p,N_p);
- for i = 0:2^N_p-1
-    u = str2num(dec2bin(i)')';
-    o(i+1,end-length(u)+1:end) =  u;
- end
-end
