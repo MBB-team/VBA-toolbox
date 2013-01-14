4a7af2db3f717ad4766829670482d7e4878b58df
jdaunize <jean.daunizeau@gmail.com>
2013-01-14 17:54:11 +0000

    - simplified fast demo scripts

--- Inversion_scripts/Demo_Delay.m
+++ Inversion_scripts/Demo_Delay.m
@@ -1,72 +1,56 @@
-% Inversion of the synthetic data from and with a hyperbolic discount
-% model with softmax decision in a binary decision task
+% This script operates the inversion of a temporal discounting model with
+% a softmax decision rule.
+% TASK DESCRIPTION:
+% At each trial, two alternatives are proposed to the agent, each of which
+% has two dimensions (reward value and delay).
+% MODEL DESCRIPTION:
+% Agents are assumed to devaluate items accroding to a hyperbolic discount
+% rate, i.e.:
+% V(d) = V0 / (1 + k*d),
+% where d is the delay, V0 is the reward magnitude, and k is the delay
+% discounting parameter. The decision is emitted according to a softmax
+% rule.
 
 close all
-clear all
+clear variables
 clc
 
-%--------------------- Task
-% Two alternatives choice task
-% - At each Trial, two alternatives are proposed by the experimenter
-%   Each of which has two dimensions (value, time of delivery)
-%   Delay between alternatives is kept constant 
-% - The Subject chooses one among the two alternatives
-%--------------------- Model
-% Hyperbolic discount v(d) = 1 / (1 + k*d) + softmax decision rule
-% Hidden states : None
-% Observed variable : chosen alternative (1*Ntrials)
-% Parameters : k, be
-%---------------------------
-
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%% INVERSION OF MODEL ONE
-
-
-g_fname = @g_1Dhyp; % observation function, Parameters : [K, beta] => n_phi = 2
-N = 100; % number of trials
-dim = struct('n',0,'n_theta',0,'n_phi',2,'p',N,'n_t',1);
-% Priors on parameters (mean and Covariance matrix)
-priors.muPhi = zeros(dim.n_phi,1); 
-priors.SigmaPhi = 1e4*eye(dim.n_phi);
-%priors.SigmaPhi(end,end) = 0; % Do not infer beta!
-% No state noise for deterministic update rules
-priors.a_alpha = Inf;
-priors.b_alpha = 0;
-% Options for inversion
-options.priors = priors;
-options.DisplayWin = 1;
-options.GnFigs = 0;
-options.binomial = 1; % Dealing with binary data
-options.dim = dim;
-options.verbose = 0;
-
-%----------------- Simulating data
-% simulation parameters
+% 1- agent parameters
 K = 1/15; % discount parameter
 beta = 0.5; % inverse temperature
-% Experimenter data
-T = zeros(2,N); % time of reception of alternatives
-T(1,:) = zeros(1,N); % random choice (uniform)  
-T(2,:) = T(1,:) + rand(1,N)*30; % T2 = T1 + random delay  
-OV = zeros(2,N); % value of alternatives
-OV(1,:) = 10; % fixed objective value
-OV(2,:) = 20; % fixed objective value
-% simulation
+N = 100; % # trials
+
+% 3- options for data simulations
+% 3.1 evolution/observation functions
+g_fname = @g_1Dhyp; % observation function, Parameters : [K, beta] => n_phi = 2
+f_fname = []; % no learning
+% 3.2 define set of alternative choices
+T = zeros(2,N); % reward delays
+T(2,:) = rand(1,N)*30;
+V = zeros(2,N); % reward values
+V(1,:) = 10;
+V(2,:) = 20;
 inG.T = T; % 2 * N (times of reception)
-inG.V = OV;  % 2 * N  (objective values)
+inG.V = V;  % 2 * N  (objective values)
+% 3.3 OPTIONS structure
+dim = struct('n',0,'n_theta',0,'n_phi',2,'p',N,'n_t',1);
+options.dim = dim;
 options.inG = inG; 
-[y,x,x0,eta,e] = simulateNLSS(N,[],g_fname,[],[log(K);log(beta)],[],Inf,Inf,options,[]);
+options.binomial = 1; % Dealing with binary data
+options.verbose = 0;
+options.inG = inG; 
+% 3.4 simulate data
+[y,x,x0,eta,e] = simulateNLSS(N,f_fname,g_fname,[],[log(K),log(beta)],[],Inf,Inf,options,[]);
 
-%-----------------Model inversion
-[posterior,out] = VBA_NLStateSpaceModel(y,[],[],g_fname,dim,options);  % Inversion function
- phi = [log(K),log(beta)]; 
-displayResults(posterior,out,y,[],[],[],phi,Inf,Inf)
+% 4- Invert model
+% 4.1 Define priors (i.i.d. normal densities)
+priors.muPhi = zeros(dim.n_phi,1); 
+priors.SigmaPhi = 1e0*eye(dim.n_phi);
+options.priors = priors;
+% 4.2 call inversion routine
+[posterior,out] = VBA_NLStateSpaceModel(y,[],f_fname,g_fname,dim,options);
+% 4.3 evaluate inversion results
+displayResults(posterior,out,y,[],[],[],[log(K),log(beta)],Inf,Inf)
 
-
-disp('//////////  Obtained parameters for hyperbolic model')
-disp([' K = ', num2str(exp(posterior.muPhi(1)))])
-disp([' beta = ', num2str(exp(posterior.muPhi(2)))])
     
     
--- Inversion_scripts/Demo_QLearning_2Q.m
+++ Inversion_scripts/Demo_QLearning_2Q.m
@@ -1,116 +1,93 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% In this script, inversion of a reinforcement learning model with
-% softmax decision rule.
-% RL model : 2 Qvalues, feedback for the selected action, Rescorla Wagner
-% update rule
-% Task : sequential decision task among two alternatives
-% that are probabilistically rewarded 
-% p(reward) = 0.8 for the first // p(reward) = 0.2 for the second
-% Probability reversal at mid course
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Demo_QLearning_2Q.m
+%%
+% This script operates the inversion of a reinforcement learning model with
+% a softmax decision rule.
+% TASK DESCRIPTION:
+% At each trial, the agent chooses between two alternative actions, which
+% are rewarded according to a predefined (probabilistic) reward schedule.
+% This is a typical two-armed bandit task.
+% MODEL DESCRIPTION:
+% The model is a typical Rescorla Wagner learning rule, whereby the action
+% value is updated proportionaly to the reward prediction error, i.e.:
+% Q(t+1) = Q(t) + alpha*(R(t)-Q(t))
+% where Q(t) is the learned value at trial t, alpha is the learning rate,
+% and R(t) is the delivered reward. Note that there are two Q-values (one
+% for each action). The decision is emitted according to a softmax rule.
 
 clear all 
 close all
 clc
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% DATA SIMULATION
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%% Simulate data
 
-% Simulate data from single session
-Ntrials = 100;
-% Simulation parameters
+% 1- Q-learner parameters
+Ntrials = 100; % # trials
 alpha = 0.2; % learning rate
-beta =1; % inverse temperature
-Q0 = [0;0];
+beta = 1; % inverse temperature
+x0 = randn(2,1); % initial Q-values
 
-% Probabilist ic reward. Correlated probabilities (complementary p1 = 1-p2)
-% Fixed probabilities (p1 =0.8) with probability reversal at mid course
-R = rand(2,Ntrials); % Rewards for each alternatives
-R(1,1:Ntrials/2) = R(1,1:Ntrials/2)<0.8;R(1,Ntrials/2+1:end) = R(1,Ntrials/2+1:end)<0.2;
-R(2,1:Ntrials/2) = R(2,1:Ntrials/2)<0.2;R(2,Ntrials/2+1:end) = R(2,Ntrials/2+1:end)<0.8;
+% 2- Define feedback.
+% NB: Here, the feedback is defined through a feedback function, which
+% delivers a reward R1 (resp. R2) to the agent when she chooses action 1
+% (resp. action 2). This is a (very simple) special case of a more general
+% procedure, by which feedback could be delivered according to a complex
+% rule.
+% NB2: this is a probabilistic reward schedule, and the reward
+% contingencies change at half of the session. 
+R = rand(2,Ntrials);
+R(1,1:Ntrials/2) = R(1,1:Ntrials/2)<0.8;
+R(1,Ntrials/2+1:end) = R(1,Ntrials/2+1:end)<=0.2;
+R(2,1:Ntrials/2) = R(2,1:Ntrials/2)<=0.2;
+R(2,Ntrials/2+1:end) = R(2,Ntrials/2+1:end)<0.8;
 
-
-% simpler R
-%R(1,:) = 1;
-%R(2,:) = 0;
-
-x0 = randn(2,1);%[0;0];
-n_t = Ntrials;
-% allocate feedback struture for simulations
-
-% For any predefined rewards for all alternatives
-h_fname = @h_reward_2Q;
-fb.inH.u0 = R;
-%[u0,~u0,u0;
-%            ~u0,u0,~u0]; % with reversals
-fb.h_fname = h_fname;
-fb.indy = 1;
-fb.indfb = [2]; % indices where put feedbacks in the experimenter data matrix u
-
+% 3- options for data simulations
+fb.inH.u0 = R; % reward scehdule
+fb.h_fname = @h_reward_2Q; % feedback function
+fb.indy = 1; % index of chosen action
+fb.indfb = 2; % index of received feedback
 f_fname = @f_Qlearn_2Q;
 g_fname = @g_softmax_2Q;
 theta = sigm(alpha,struct('INV',1));
 phi = [log(beta);0.5];
-u = [zeros(2,Ntrials)];
-
+u = [zeros(max([fb.indfb(:);fb.indy(:)]),Ntrials)];
 alpha = Inf;
 sigma =  Inf;
-
-dim_output = 1; % the choice
-dim = struct('n',2,...  %( 2 (Qvalues) 
-             'p',dim_output,... % total output dimension
-             'n_theta',1,... % evolution parameters
-             'n_phi', 2,... % observation parameters
-             'n_t',Ntrials);
-
-
-options.inF.param_transform.type = 'modified sigmoid';
-options.inF.param_transform.a = 0;
-options.inF.param_transform.b = 1;
-
-options.inG.param_transform.type = 'modified sigmoid';
-options.inG.param_transform.a = 1;
-options.inG.param_transform.b = 5;
-
-options.DisplayWin = 1;
-options.GnFigs = 0;
+in.param_transform.type = 'modified sigmoid';
+in.param_transform.a = 0;
+in.param_transform.b = 1;
+options.inF = in;
+options.inG = in;
 options.binomial = 1; % Dealing with binary data
 options.isYout = zeros(1,Ntrials); % Excluding data points
+dim = struct('n',2,...  % hidden states = Q-values
+    'p',1,... % output = emitted choices
+    'n_theta',1,... % evolution parameters
+    'n_phi', 2,... % observation parameters
+    'n_t',Ntrials);
 options.dim = dim;
-                
-options.skipf = zeros(1,n_t);
+options.skipf = zeros(1,Ntrials);
 options.skipf(1) = 1; % apply identity mapping from x0 to x1.
 
-[y,x,x0,eta,e,u] = simulateNLSS_fb(n_t,f_fname,g_fname,theta,phi,u,Inf,Inf,options,x0,fb);
+% 4- simulate and display data
+[y,x,x0,eta,e,u] = simulateNLSS_fb(Ntrials,f_fname,g_fname,theta,phi,u,Inf,Inf,options,x0,fb);
+displaySimulations(y,x,eta,e)
 
-plot(x')
-%%
 
-% Defining Priors
-% Priors on parameters (mean and Covariance matrix)
+%% Invert model
+
+% 1- Define priors (i.i.d. normal densities)
 priors.muPhi = zeros(dim.n_phi,1); 
-priors.muTheta = zeros(dim.n_theta,1);
 priors.SigmaPhi = 1e0*eye(dim.n_phi);
+priors.muTheta = zeros(dim.n_theta,1);
 priors.SigmaTheta = 1e0*eye(dim.n_theta);
-% priors.SigmaPhi(2,2) = 0;
-% Priors on initial 
 priors.muX0 = ones(dim.n,1)*0;
-priors.SigmaX0 =0e0*eye(dim.n);
-% No state noise for deterministic update rules
-priors.a_alpha = Inf;
+priors.SigmaX0 =1e0*eye(dim.n);
+priors.a_alpha = Inf; % No state noise for deterministic update rules
 priors.b_alpha = 0;
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% MODEL INVERSION
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%%%% Options for inversion
-options.DisplayWin = 1;
-options.GnFigs = 0;
-options.binomial = 1; % Dealing with binary data
 options.priors = priors;
 
+% 2- call inversion routine
 [posterior,out] = VBA_NLStateSpaceModel(y,u,f_fname,g_fname,dim,options);
 
+%% evaluate inversion results
 displayResults(posterior,out,y,x,x0,theta,phi,Inf,Inf)
