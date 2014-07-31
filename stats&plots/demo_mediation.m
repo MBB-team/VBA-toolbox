05d13cd02ddfa41f6a123a318281a07852386db3
Jean Daunizeau <jean.daunizeau@gmail.com>
2014-07-31 11:55:01 +0200

    added classical mediation analysis
    classical mediation analysis (Baron&Kenny's 3-steps approach + Sobel
    test)

---
+++ stats&plots/demo_mediation.m
@@ -0,0 +1,15 @@
+% this demonstrates Baron & Kenny's simple mediation analysis
+
+close all
+clear all
+clc
+
+n = 32; % sample size
+s = 2; % noise standard deviation
+
+X = randn(n,1) + s*10*randn;
+M = X + s*randn(n,1) + s*randn;
+Y = M + s*randn(n,1) + s*randn;
+
+
+[out] = mediationAnalysis0(Y,X,M);
