81d4079f427364a304b3061a87a913b7fd1d5fba
lionel-rigoux <lionel.rigoux@gmail.com>
2014-09-22 12:47:29 +0200

    cleaner getSuffstat

--- VBA_getSuffStat.m
+++ VBA_getSuffStat.m
@@ -1,120 +1,35 @@
 function [suffStat] = VBA_getSuffStat(options,suffStat,flag)
 % fills in default sufficient statistics structure
-% function [suffStat] = VBA_getSuffStat(options)
+% function [suffStat] = VBA_getSuffStat(options,suffStat,flag)
 % IN:
-%   - options: the options structure (only the substructure .dim is used
-%   here)
+%   - options: the options structure (only the substructure .dim is used here)
 %   - suffStat: {[]}
 %   - flag: {0}, 1 if online version
 
-try
-    flag;
-catch
-    flag = 0;
-end
-try
-    suffStat.F;
-catch
-    suffStat.F = [];
-end
-try
-    suffStat.gx;
-catch
-    suffStat.gx = zeros(options.dim.p,options.dim.n_t);
-end
-try
-    suffStat.vy;
-catch
-    suffStat.vy = zeros(options.dim.p,options.dim.n_t);
-end
-try
-    suffStat.muX;
-catch
-    suffStat.muX = zeros(options.dim.n,options.dim.n_t);
-end
-try
-    suffStat.dy;
-catch
-    suffStat.dy = zeros(options.dim.p,options.dim.n_t);
-end
-try
-    suffStat.dx;
-catch
-    suffStat.dx = zeros(options.dim.n,options.dim.n_t);
-end
-try
-    suffStat.vdx;
-catch
-    suffStat.vdx = zeros(options.dim.n,options.dim.n_t);
-end
-try
-    suffStat.dx0;
-catch
-    suffStat.dx0 = zeros(options.dim.n,1);
-end
-try
-    suffStat.dy2;
-catch
-    suffStat.dy2 = zeros(1,numel(options.sources));
-end
-try
-    suffStat.dx2;
-catch
-    suffStat.dx2 = 0;
-end
-try
-    suffStat.dtheta;
-catch
-    if ~flag
-        suffStat.dtheta = zeros(options.dim.n_theta,1);
-    else
-        suffStat.dtheta = zeros(options.dim.n_theta,options.dim.n_t);
-    end
-end
-try
-    suffStat.dphi;
-catch
-    if ~flag
-        suffStat.dphi = zeros(options.dim.n_phi,1);
-    else
-        suffStat.dphi = zeros(options.dim.n_phi,options.dim.n_t);
-    end
-end
-try
-    suffStat.Ssigma;
-catch
-    suffStat.Ssigma = 0;
-end
-try
-    suffStat.Sphi;
-catch
-    suffStat.Sphi = 0;
-end
-try
-    suffStat.Stheta;
-catch
-    suffStat.Stheta = 0;
-end
-try
-    suffStat.Salpha;
-catch
-    suffStat.Salpha = 0;
-end
-try
-    suffStat.SX;
-catch
-    suffStat.SX = 0;
-end
-try
-    suffStat.SX0;
-catch
-    suffStat.SX0 = 0;
-end
+if ~exist('flag','var'), flag = 0; end
 
-try
-        suffStat.logL;
-catch
-        suffStat.logL = -Inf;
-end
+dim = options.dim;
+dim.online = elvis(flag, dim.n_t, 1); 
+
+suffStat = check_struct(suffStat, ...
+    'F'     , []                                    , ...
+    'gx'    , zeros(dim.p,dim.n_t)                  , ...
+    'vy'    , zeros(dim.p,dim.n_t)                  , ...
+    'muX'   , zeros(dim.n,dim.n_t)                  , ...
+    'dy'    , zeros(dim.p,dim.n_t)                  , ...
+    'dx'    , zeros(dim.n,dim.n_t)                  , ...
+    'vdx'   , zeros(dim.n,dim.n_t)                  , ...
+    'dx0'   , zeros(dim.n,1)                        , ...
+    'dy2'   , zeros(1,numel(options.sources))       , ...
+    'dx2'   , 0                                     , ...
+    'dtheta', zeros(dim.n_theta,dim.online)         , ...
+    'dphi'  , zeros(dim.n_phi  ,dim.online)         , ...
+    'Ssigma', 0                                     , ...
+    'Sphi'  , 0                                     , ...
+    'Stheta', 0                                     , ...
+    'Salpha', 0                                     , ...
+    'SX'    , 0                                     , ...
+    'SX0'   , 0                                     , ...
+    'logL'  , -Inf                                    ...
+);
     
-
---
+++ subfunctions/elvis.m
@@ -0,0 +1,10 @@
+function value = elvis(cond,a,b)
+% ELVIS
+% value = elvis(cond,a,b)
+% if cond, then value = a, else value = b
+
+if cond
+    value = a;
+else
+    value = b;
+end
