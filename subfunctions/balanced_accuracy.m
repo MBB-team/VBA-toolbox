3ad0b971a9db8488ec926432fb3fa08f7e1ad8df
lionel-rigoux <lionel.rigoux@gmail.com>
2014-09-23 13:27:54 +0200

    corrected balanced accuracy computation

--- subfunctions/balanced_accuracy.m
+++ subfunctions/balanced_accuracy.m
@@ -1,7 +1,21 @@
-function acc=balanced_accuracy(pred,data)
-pred=vec(pred);
-data=vec(data);
-    pos_idx = find(pred>=.5);
-    neg_idx = find(pred<=.5);    
-    acc = mean( [mean(data(pos_idx)) , mean(1-data(neg_idx))] );
+function accuracy=balanced_accuracy(pred,data,isOut)
+
+if ~exist('isOut','var')
+    isOut = zeros(size(pred));
+end
+
+pred=vec(pred(isOut==0));
+data=vec(data(isOut==0));
+
+
+bg = pred > 0.5;                   % binarized model predictions
+tp = sum(   data   .*   bg    );   % true positives
+fp = sum( (1-data) .*   bg    );   % false positives
+fn = sum(   data   .* (1-bg)  );   % false positives
+tn = sum( (1-data) .* (1-bg)  );   %true negatives
+P = tp + fn;
+N = tn + fp;
+
+accuracy = 0.5*(tp./P + tn./N);
+
 end
