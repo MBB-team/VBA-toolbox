97b140ca34092a49797a5ae877bf0ce249fc8aa7
lionel-rigoux <lionel.rigoux@gmail.com>
2014-10-03 09:39:34 +0200

    prior syntactic sugar

---
+++ subfunctions/priorPrettifyer.m
@@ -0,0 +1,9 @@
+function s=priorPrettifyer(labeler,muPriors)
+
+names = fieldnames(labeler);
+
+% s=[];
+for i=1:numel(names)
+    s.(names{i}) = muPriors(labeler.(names{i}),:)';
+%     s=[s sprintf('%s:\t\t%s \n',names{i},num2str(muPriors(labeler.(names{i}),:)'))];
+end
---
+++ subfunctions/priorUglyfier.m
@@ -0,0 +1,33 @@
+function [priorsMu, priorsSigma,labeler] = priorUglyfier(varargin)
+
+
+
+n = numel(varargin);
+assert(mod(n,3)==0,'***Parameters should be: ''name'',mu,sigma,...');
+
+priorsMu = [];
+priorsSigma = [];
+
+cpt = 0;
+for i=1:3:n
+    
+    name = varargin{i};
+    mu = varargin{i+1};
+    sigma = varargin{i+2};
+    
+    
+    nPar = numel(mu);
+    par_idx = cpt+(1:nPar);
+    cpt = cpt + nPar;
+
+    labeler.(name)=par_idx ;
+    
+    priorsMu(par_idx) = mu;
+    
+    priorsSigma(par_idx,par_idx) = diag(sigma);
+
+    
+    
+end
+priorsMu = priorsMu(:);
+    
