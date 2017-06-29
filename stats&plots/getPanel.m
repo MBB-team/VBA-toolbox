b40f6c2c01fb6cfc76ba667db1818c8c017774de
lionel.rigoux@sf.mpg.de <lionel.rigoux@gmail.com>
2017-06-29 17:33:58 +0200

    solve bug in display of online inversion in  Matlab < 2014b

--- stats&plots/getPanel.m
+++ stats&plots/getPanel.m
@@ -1,13 +1,9 @@
 function hfig = getPanel(hfig)
-    if verLessThan('matlab','8.4.0')
-        return
-    else % in 2014b gco parent is the figure not the panel...
-%         hpanel = findobj(hfig,'Type','Panel');
-        c = get(hfig,'Children');
-        isPanel = cell2mat(arrayfun(@(x) strcmp(class(x),'matlab.ui.container.Panel'),c,'uniformOutput',false));
-        hpanel = c(isPanel);
-        if ~isempty(hpanel)
-            hfig = hpanel ;
-        end
-        
+
+    c = get(hfig,'Children');
+    isPanel = cell2mat(arrayfun(@(x) strcmp(get(x,'Type'),'uipanel'),c,'uniformOutput',false));
+    hpanel = c(isPanel);
+    if ~isempty(hpanel)
+        hfig = hpanel ;
     end
+
