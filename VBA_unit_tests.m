7a1283ac3f9169cf3d596ee51a6d17188414ec6f
lionel.rigoux@sf.mpg.de <lionel.rigoux@gmail.com>
2017-07-07 15:06:53 +0200

    improve unit tests procedure

--- VBA_unit_tests.m
+++ VBA_unit_tests.m
@@ -1,4 +1,4 @@
-function log = VBA_unit_tests()
+function logs = VBA_unit_tests(logs)
 % This function simply launches sequentially all the demos of the toolbox and
 % reports execution time and potential failures
 
@@ -15,9 +15,9 @@ end
 
 
 %% run demos
-log = struct('demo',{},'status',{},'stack',{},'time',{});
+logs = struct('demo',{},'status',{},'stack',{},'time',{});
 
-for i = 1:numel(demos)
+parfor i = 1:numel(demos)
    demo_name = demos{i};
    
    try
@@ -26,13 +26,11 @@ for i = 1:numel(demos)
        fprintf('\n ####################\n  %s \n ####################\n',demo_name)
        tic ;
        evalin('base',demo_name)
-       log(i) = struct('demo',demo_name,'status',1,'stack',[],'time',toc);
+       logs(i) = struct('demo',demo_name,'status',1,'stack',[],'time',toc);
    catch err
-       log(i) = struct('demo',demo_name,'status',0,'stack',err,'time',toc);
+       logs(i) = struct('demo',demo_name,'status',0,'stack',err,'time',toc);
    end
-
    
-   save(['../VBA_unit_test_log_' version('-release')],'log')
-       
 end
 
+save(['../VBA_unit_test_log_' version('-release')],'log')
