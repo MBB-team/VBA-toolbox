26e442cc0971bf6e5079f495ad4bf5e10c80a76f
lionel.rigoux@sf.mpg.de <lionel.rigoux@gmail.com>
2018-01-25 19:03:51 +0100

    fix bug in demo_influencelearning

--- subfunctions/demo_influenceLearning.m
+++ subfunctions/demo_influenceLearning.m
@@ -50,7 +50,7 @@ options.priors.SigmaTheta = 1e2*eye(dim.n_theta);
 f_fname = @f_Hampton;
 g_fname = @g_Hampton;
 u = [zeros(2,1),[o;a]];
-[posterior,out] = VBA_hyperparameters(a,u,f_fname,g_fname,dim,options);
+[posterior,out] = VBA_NLStateSpaceModel(a,u,f_fname,g_fname,dim,options);
 
 
 
