05ed06ce006491d948daf3f6403e39811eeb0a40
lionel.rigoux@sf.mpg.de <lionel.rigoux@gmail.com>
2018-05-03 17:46:20 +0200

    isInRange accepts empty bounds

--- subfunctions/VBA_isInRange.m
+++ subfunctions/VBA_isInRange.m
@@ -6,13 +6,19 @@ function [flag] = VBA_isInRange (X, bounds)
 %
 % IN:
 %   - X: matrix, cell array of matrices, or structure to be checked
-%   - bounds: a 2x1 vector that defines an interval on the real line
+%   - bounds: a 2x1 vector that defines an interval on the real line. If
+%     empty, the return flag will always be true
 % OUT:
 %   - flag: - true if all element and fields of X are within the bounds
 %           - false if at least one element is outside the bounds 
 %           - NaN if any element or field is not numeric or weird
 %
 % /////////////////////////////////////////////////////////////////////////
+
+if isempty (bounds)
+    flag = true;
+    return;
+end
 
 if VBA_isWeird (X)
     flag = NaN;
