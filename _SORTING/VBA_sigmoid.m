bee3c886e9c3e40bc37aedfa54f35ce11c403a9e
Unknown <lionel.rigoux@sf.mpg.de>
2018-05-05 18:15:30 +0200

    improved sigmoid function

--- _SORTING/VBA_sigmoid.m
+++ _SORTING/VBA_sigmoid.m
@@ -1,9 +1,52 @@
 function [y, dsdx, dsdp] = VBA_sigmoid(x, varargin)
+% // VBA toolbox //////////////////////////////////////////////////////////
+%
+% [y, dsdx, dsdp] = VBA_sigmoid(x, [name, value, ...])
+% Apply a sigmoid transformation to x
+%
+% By default, the canonical sigmoid is used:
+%
+%   y = 1 / (1 + exp(- x))
+%
+% However, this function can be parametrized using name/value pairs of
+% arguments to get, using all options:
+%
+%   y = offset + scale * 1 / (1 + exp(- slope * (x - center))
+%
+% IN:
+%   - x: values to be transformed
+%   - optional key/value pairs or structure (or both) that parametrize the 
+%     sigmoid transformation:
+%       - 'slope'     : inverse-temperature parameter
+%       - 'center'    : absissa of the inflexion point
+%       - 'scale'     : multiplicative gain of the transformation
+%       - 'offset'    : additive gain 
+%       - 'lapseRate' : shotcut for offset = lapseRate and scale = 1 - 2 * lapseRate
+%                       this option is incompatible with offset or scale
+%       - 'inverse'   : reversed transformation, ie. x = VBA_sigmoid(y, opt)
+% 
+%   Note that if the 'inverse' flag is set to true, the derivatives will
+%   not be computed and dsdx = dsdp = [] will be returned instead.
+%
+% OUT:
+%   - y: transformed values, with the same dimension as x.
+%   - dsdx: derivative of the transformation wrt. x, taken at each point x.
+%     Has the same dimension as x.
+%   - dsdp: derivative of the transformation wrt. the parameters specified
+%     as arguments. Derivatives are taken for parameters in alphabetical
+%     order and aggregated along the first dimension. For example, calling
+%     VBA_sigmoid([x1; x2], 'slope', 2, 'center', 3) will return dsdp as:
+%     [ d_s/d_center(x1) d_s/d_center(x2) ;
+%       d_s/d_slope(x1)  d_s/d_slope(x2)  ]
+%     If x is multidimensional, size(dsdp) = [nb_params, size(x,1), size(x,2), ...]
+%
+% /////////////////////////////////////////////////////////////////////////
+
 
 %% 
-epsilon = 1e-8;
+% epsilon = 1e-8;
 
-%% specify arguments
+%% parse arguments
 parser = inputParser;
 parser.PartialMatching = false;
 
@@ -27,20 +70,23 @@ if ~ ismember ('lapseRate', parser.UsingDefaults)
     params.offset = params.lapseRate;
     params.scale = 1 - 2 * params.lapseRate;
 end
+
+
 %% compute
     
 if params.inverse
     % evaluate inverse sigmoid  
-    x = params.scale .* (x - params.offset) .^-1  - 1;
-    y = params.center - params.slope .^-1 .* log (x)  ;
+    lx = params.scale .* (x - params.offset) .^-1  - 1;
+    y = params.center - params.slope .^-1 .* log (lx)  ;
     
     % skip derivatives, they are generally not used
     dsdx = [];
     dsdp = [];
 else
     % evaluate sigmoid
-    x = params.slope * (x - params.center);
-    y = params.offset + params.scale ./ (1 + exp(- x));
+    lx = params.slope * (x - params.center);
+    sx = 1 ./ (1 + exp(- lx));
+    y = params.offset + params.scale * sx;
     
     % ensure finite precision
 %     minY = params.offset + epsilon;
@@ -64,21 +110,21 @@ else
         return
     end
 
-    dsdp = [];
+    dims = size(x);
     
-% pdiff = setdiff(parser.Parameters, parser.UsingDefaults);
-% 
-% % evaluate derivatives wrt parameters
-% if size(Phi,1) >= 1
-%     dsdp = zeros(size(Phi,1),length(x));
-%     dsdp(1,:) = (x-th).*in.beta.*dsdx;
-%     if size(Phi,1) == 2
-%         dsdp(2,:) = -dsdx;
-%     end
-%     dsdp(isnan(dsdp)) = 0;
-% else
-%     dsdp = [];
-% end
+    dsdp = cat (2, ...
+        - vec (dsdx), ... % d_center
+        1 - 2 * vec (sx), ... d_lapseRate
+        ones(numel(x),1), ... d_offset
+        vec (sx), ... d_scale
+        ((vec (x) - params.center) / params.slope) .* vec (dsdx) ... d_slope
+        );
+
+    idxDefault = ismember ({'center','lapseRate','offset','scale','slope'}, parser.UsingDefaults);
+    
+    dsdp(:,idxDefault) = [];
+    
+    dsdp = reshape(dsdp', [size(dsdp,2) dims]);
 
 
 end
