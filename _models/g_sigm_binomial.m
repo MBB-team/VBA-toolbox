0e1897bfe4746670a03886808ecac26f4a4c8d29
lionel.rigoux@sf.mpg.de <lionel.rigoux@gmail.com>
2018-05-07 17:24:05 +0200

    remove sigm again

--- _models/g_sigm_binomial.m
+++ _models/g_sigm_binomial.m
@@ -1,18 +1,24 @@
 function [Sx,dsdx,dsdp] = g_sigm_binomial(x,Phi,u,in)
 % evaluates the sigmoid function for binomial data analysis
 
-in.G0 = 1;
-in.S0 = 0;
-in.beta = 1;
-in.INV = 0;
-try,in.x;catch,in.x = 0;end
+try
+    in.x;
+catch
+    in.x = 0;
+end
+
 if in.x % for learning effects (sigmoid parameters evolve over time)
-    [Sx,dsdx,dsdp] = sigm(u,in,x);
-    dsdx = dsdp;
+    [Sx, ~, dsdx] = VBA_sigmoid(u,'slope',exp(x(1)),'center',x(2));
+    
+    dsdx = flipud(dsdx);
+    dsdx(1,:) = dsdx(1,:) * exp(x(1));  
     dsdp = [];
+   
 else
-    [Sx,dsdx,dsdp] = sigm(u,in,Phi);
-    dsdx = [];
+    [Sx, ~, dsdp] = VBA_sigmoid(u,'slope',exp(Phi(1)),'center',Phi(2));
+   
+    dsdp = flipud(dsdp);
+    dsdp(1,:) = dsdp(1,:) * exp(Phi(1)); 
 end
 
 
