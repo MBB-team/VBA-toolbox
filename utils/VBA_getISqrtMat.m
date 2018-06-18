ecda297ba6b46a0903e9ab01dd22fae6678535ba
lionel.rigoux@sf.mpg.de <lionel.rigoux@gmail.com>
2018-06-18 09:49:57 +0200

    legacy fallback

--- utils/VBA_getISqrtMat.m
+++ utils/VBA_getISqrtMat.m
@@ -1,34 +1,11 @@
 function S = VBA_getISqrtMat(C,inv)
-% quick derivation of the (inverse) square root of a matrix
-% function S = VBA_getISqrtMat(C,inv)
-% IN:
-%   - C: the entry matrix
-%   - inv: binary flag for inverse square root (inv=1) or square root
-%   (inv=0) matrix operator
-% OUT:
-%   - S: the (inverse) square root of C.
+% legacy code
+s = warning ('on');
+warning ('*** The function `VBA_getISqrtMat` is now deprecated. Please see `VBA_sqrtm` for an alternative.') 
+warning (s);
 
+% fallback
 if nargin < 2
-    inv = 1;
-else
-    inv = ~~inv;
+    inv = true;
 end
-C(C==Inf) = realmax;  % dirty fix for infinite precision matrices
-if sum(C(:)) ~= 0
-    if isequal(C,diag(diag(C)))
-        if inv
-            S = diag(sqrt(diag(C).^-1));
-        else
-            S = diag(sqrt(diag(C)));
-        end
-    else
-        [U,s,V] = svd(full(C));
-        if inv
-            S = U*diag(sqrt(diag(s).^-1))*V';
-        else
-            S = U*diag(sqrt(diag(s)))*V';
-        end
-    end
-else
-    S = 0;
-end
+S = VBA_getISqrtMat(C, inv);
