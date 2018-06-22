6e5a46689065efda7f63ade2dfa3107d54f54f6e
lionel.rigoux@sf.mpg.de <lionel.rigoux@gmail.com>
2018-06-22 11:22:51 +0200

    refactor demo Volterra

--- demos/_models/f_rwl.m
+++ demos/_models/f_rwl.m
@@ -1,5 +1,7 @@
 function fx = f_rwl(x,P,u,in)
 fx = x;
-for i=1:size(u,1)
-    fx = fx + P(i)*(u(i)-x);
+for i = 1 : size (u, 1)
+    if ~ VBA_isWeird (u(i))
+        fx = fx + P(i) * (u(i) - x);
+    end
 end
