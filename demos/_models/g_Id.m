d1a8b14c9eb35cbdc2908782c47d4980ced896a6
lionel.rigoux@sf.mpg.de <lionel.rigoux@gmail.com>
2018-06-25 16:36:08 +0200

    faster g_Id

--- demos/_models/g_Id.m
+++ demos/_models/g_Id.m
@@ -1,30 +1,28 @@
 function [gx,dG_dX,dG_dPhi] = g_Id(Xt,Phi,ut,inG)
 % Identity observation mapping (partially observable)
 
-n = size(Xt,1);
-try
-    ind = inG.ind;
-catch
-    ind = 1:n;
-end
-try
-    scale = inG.scale;
-catch
-    scale = 1;
-end
-G = eye(n);
-G = scale*G(ind,:);
+n = size (Xt, 1);
 
-try
-    G = kron(G,ones(inG.k,1));
+if ~ isfield (inG, 'ind')
+    inG.ind = 1:n;
+end
+if ~ isfield (inG, 'scale')
+    inG.scale = 1;
+end
+
+G = eye (n);
+G = inG.scale * G(inG.ind, :);
+
+if isfield(inG, 'k')
+    G = kron (G, ones(inG.k, 1));
 end
 
 
-gx = G*Xt;
+gx = G * Xt;
 dG_dX = G';
 
-if size(Phi,1) > 0
-    dG_dPhi = zeros(size(Phi,1),size(G,1));
+if size (Phi, 1) > 0
+    dG_dPhi = zeros (size (Phi, 1), size(G, 1));
 else
     dG_dPhi = [];
 end
