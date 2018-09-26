1ab1f9a6c3aef69437219c7ec281e22f5d1a02eb
lionel.rigoux@sf.mpg.de <lionel.rigoux@gmail.com>
2018-09-26 16:36:05 +0200

    fallback

---
+++ legacy/VBA_ExceedanceProb.m
@@ -0,0 +1,38 @@
+function ep = VBA_ExceedanceProb (mu, Sigma, form, verbose, Nsamp)
+
+% legacy code
+s = warning ('on');
+warning ('*** The function `VBA_ExceedanceProb` is now deprecated. Please see `VBA_exceedanceProbability` for an alternative.') 
+warning (s);
+
+options = struct ();
+
+try
+    form;
+catch
+    form = 'gaussian';
+end
+
+if form(end) == '2'
+    form = form(1:end-1);
+    options.method = 'analytical';
+else
+    options.method = 'sampling';
+end
+
+switch form
+    case 'gaussian'
+        assert(numel(mu) == numel(Sigma),'Sigma must be the same size as mu for Gaussian densities');
+    case 'dirichlet'
+        assert(isempty(Sigma),'Sigma must be empty for Dirichlet densities');
+end
+
+try
+    options.verbose = verbose;
+end
+
+try
+    options.nSamples = Nsamp;
+end
+
+ep = VBA_exceedanceProbability (mu, Sigma, options);
