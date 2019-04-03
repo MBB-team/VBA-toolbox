1f030faaa9b476030323f4eb3b98df18a3fa06a6
lionel.rigoux@sf.mpg.de <lionel.rigoux@gmail.com>
2019-04-03 13:53:39 +0200

    fix #64

--- utils/VBA_orth.m
+++ utils/VBA_orth.m
@@ -1,4 +1,4 @@
-function A = VBA_orth (A, norm, nanflag)
+function A = VBA_orth (A, norm)
 % serial orthogonalisation of matrix A
 % function A = VBA_orth(A,norm)
 % This function considers each column serially, projecting it in the null
@@ -13,21 +13,21 @@ if nargin < 2
     norm = false;
 end
 
-if nargin < 3
-    nanflag = 'includenan';
+if VBA_isWeird(A) 
+    error('***VBA_orth: your matrix should not include any weird values (Nans, Inf, etc).');
 end
 
 [n,p] = size(A);
 I = eye(n);
 
 if norm
-    A(:,1) = VBA_zscore (A(:, 1), [], [], nanflag);
+    A(:,1) = VBA_zscore (A(:, 1));
 end
 for i=2:p
     X = A(:, 1 : i - 1);
     nX = I - X * pinv(X' * X) * X';
     A(:,i) = nX * A(:,i);
     if norm
-        A(:,i) = VBA_zscore (A(:,i), [], [], nanflag);
+        A(:,i) = VBA_zscore (A(:,i));
     end
 end
