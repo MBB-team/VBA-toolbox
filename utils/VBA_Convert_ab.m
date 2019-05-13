815597a47c179c1a08923972cede15c78a792257
manueich <m.eichenlaub@warwick.ac.uk>
2019-05-13 14:25:33 +0100

    Add files via upload

---
+++ utils/VBA_Convert_ab.m
@@ -0,0 +1,29 @@
+function [mu,sig] = VBA_Convert_ab(a,b)
+% [mu,sig] = Convert_ab(a,b)
+%
+% Converts a and b defining a gamma distributed precision p to mean (mu) and
+% standard deviation (sig) of the distribution over the associated standard
+% deviation s, i.e. s = 1/sqrt(p)
+% IN:
+%   a,b: Can either be a_sigma/b_sigma or a_alpha/b_alpha defining
+%   posterior distributions over measurement/system noise precision
+% OUT:
+%   mu,sig: mean and standard deviation of distribution over s
+% 
+% Be aware that this is only possible for a>1
+%
+%  M. Eichenlaub 13/05/2019
+
+if isinf(a) % Return 0 if system noise was switched off, i.e. a_sigma = Inf
+    mu=0;
+    sig = 0;
+elseif a>1
+    mu = sqrt(b)*exp(gammaln(a-0.5)-gammaln(a));
+    sig = sqrt(b/(a-1)-b*exp(2*(gammaln(a-0.5)-gammaln(a))));
+else
+    disp('Conversion error: a<=1');
+end
+
+ 
+
+
---
+++ utils/VBA_Create_NoisePrior.m
@@ -0,0 +1,50 @@
+function [ a,b ] = VBA_Create_NoisePrior( mu,sig )
+%[ a,b ] = Create_NoisePrior( mu,sig )
+%
+% Converts mu and sig defining a prior distribution over noise standardard
+% deviation s, to a gamma distriribution over the associated precision p
+% defined by a and b with p = 1/s^2
+
+% IN:
+%   mu,sig: mean and standard deviation of prior distribution over s
+% OUT:
+%   a,b: Can either be a_sigma/b_sigma or a_alpha/b_alpha defining
+%   prior distributions over measurement/system noise precision
+% 
+%  M. Eichenlaub 13/05/2019
+
+if nargin==1 % Return this if system noise shall be switched off
+    a = Inf;
+    b = 0;
+else
+
+% Starting value
+a0 = 1/8*(1+sqrt(49+mu^4/sig^4+50*mu^2/sig^2)+mu^2/sig^2);
+
+% Find a
+options = optimoptions('fmincon','Display','none','Tolfun',1e-8,'TolX',1e-8);
+a = fmincon(@(x)D(x,mu,sig),a0,[],[],[],[],1,Inf,[],options);
+
+b = (mu./(exp(gammaln(a-0.5)-gammaln(a)))).^2;
+
+[m,s] = Convert_ab(a,b);
+
+% Check results, reject if error on either mu or sig is greater than 1%
+if abs(m-mu)/mu>1e-2 || abs(s-sig)/mu>1e-2
+    a=0;
+    b=0;
+    disp('optimization Error');
+end
+
+end
+
+end
+
+function [ D ] = D(a,mu,sig)
+% Function has to be minimized with respect to a
+
+S = exp(2*(gammaln(a-0.5)-gammaln(a)));
+D = mu^2./S - sig^2./((1./(a-1))-S);
+D = log(D.^2+1);
+
+end
